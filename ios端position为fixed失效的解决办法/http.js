07c74dc3a85fd6b223f3cae3b5d64d7c2f9b407c
zuobaiquan <zuobaiquan@qq.com>
2018-09-29 18:05:50 +0800

    dd

--- ios端position为fixed失效的解决办法/http.js
+++ ios端position为fixed失效的解决办法/http.js
@@ -3,40 +3,38 @@ var PORT = 3000;//
 var http = require('http');
 var url=require('url');
 var fs=require('fs');
-var mine=require('./mine').types;//
+var mine=require('./mine').types;
 var path=require('path');
 
-var server = http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
+http.createServer(function(req,res){
+    var pathname = url.parse(req.url).pathname;
     var realPath = path.join("./", pathname);    //这里设置自己的文件名称;
-    console.log(realPath);
     var ext = path.extname(realPath);
-    ext = ext ? ext.slice(1) : 'unknown';
+    ext = ext ? '.'+ext.slice(1) : 'unknown';
     fs.exists(realPath, function (exists) {
         if (!exists) {
-            response.writeHead(404, {
+            res.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
-            response.write("This request URL " + pathname + " was not found on this server.");
-            response.end();
+            res.write("This req URL " + pathname + " was not found on this server.");
+            res.end();
         } else {
             fs.readFile(realPath, "binary", function (err, file) {
                 if (err) {
-                    response.writeHead(500, {
+                    res.writeHead(500, {
                         'Content-Type': 'text/plain'
                     });
-                    response.end(err);
+                    res.end(err);
                 } else {
                     var contentType = mine[ext] || "text/plain";
-                    response.writeHead(200, {
+                    res.writeHead(200, {
                         'Content-Type': contentType
                     });
-                    response.write(file, "binary");
-                    response.end();
+                    res.write(file, "binary");
+                    res.end();
                 }
             });
         }
     });
-});
-server.listen(PORT);
+}).listen(PORT);
 console.log("Server runing at port: " + PORT + ".");
