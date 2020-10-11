4d85d2c3285080268e4279356f6ac52afcf6d2ee
zuobaiquan <zuobaiquan@qq.com>
2020-10-11 11:31:40 +0800

    ✨feat: update

--- typescript code/01基础/index.ts
+++ typescript code/01基础/index.ts
@@ -1,1 +1,23 @@
-const count: number = 2019;
+// 基础类型 null, undefined, symbol, boolean, void
+
+const count: number = 2020;
+const myName: string = 'zuobaiquan';
+
+// 对象类型
+const teacher: {
+  name: string;
+  age: number;
+} = {
+  name: 'zuobaiquan',
+  age: 20,
+};
+
+const numbers: number[] = [12, 4];
+
+class Person {}
+
+const zuo: Person = new Person();
+
+const getTotal: () => number = () => {
+  return 12;
+};
