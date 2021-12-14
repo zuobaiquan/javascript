afffcebf56009440b8863cf2ee342c65f16e93e2
zuobaiquan <zuobaiquan@qq.com>
2021-12-14 10:59:54 +0800

    fix: d

---
+++ patch-package/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <el-input v-model="input" charLength maxlength="6" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      input:''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
