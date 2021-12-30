ce93d6a513f1681be6db42f9c82852c2e07d582c
zuobaiquan <zuobaiquan@qq.com>
2021-12-30 21:20:40 +0800

    fix: f

--- cli/es-parser/src/index.js
+++ cli/es-parser/src/index.js
@@ -1,2 +1,36 @@
-let sum = (x, y) => x + y
-console.log(sum(4, 5))
+const arr = [12, 322, 0, 12, 12, 78, 12, 32]
+
+// function unique1(array) {
+//   const result = []
+//   for (let i = 0; i < array.length; i++) {
+//     if (result.indexOf(array[i]) == -1)
+//       result.push(array[i])
+//   }
+//   return result;
+// }
+// console.log(unique1(arr))
+
+// es6方法数组去重
+// console.log([...new Set(arr)])
+// console.log(Array.from(new Set(arr)))  //Array.from()能把set结构转换为数组
+
+
+// function unique4(arrry) {
+//   const result = []
+//   const tmp = {}
+//   for (var i = 0; i < arrry.length; i++) {
+//     if (!tmp[arrry[i]]) {
+//       tmp[arrry[i]] = 1
+//       result.push(arrry[i])
+//     }
+//   }
+//   return result
+// }
+// console.log(unique4(arr))
+
+function unique5(array) {
+  return array.filter((item, index) => {
+    return array.indexOf(item) == index
+  })
+}
+console.log(unique5(arr))
