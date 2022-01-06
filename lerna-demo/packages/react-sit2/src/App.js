1befd21b3ab042ac6acb1d2859fd5150c43225db
zuobaiquan <zuobaiquan@qq.com>
2022-01-06 17:09:55 +0800

    fix

--- lerna-demo/packages/react-sit2/src/App.js
+++ lerna-demo/packages/react-sit2/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, memo } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Foo = memo(function Foo(props) {
+  console.log('Foo reander');
+  return <div>子组件person.age的值：{props.person.age}</div>;
+})
+
+class App extends Component {
+
+  state = {
+    count: 0,
+    person: {
+      age: 18
+    }
+  };
+
+  callback = () => { }
+
+  render() {
+    const person = this.state.person;
+    return (
+      <div className="app">
+        <p>count值：{this.state.count}</p>
+        <Foo person={person} cb={this.callback} />
+        <button
+          onClick={() => {
+            person.age++
+            this.setState({
+              count: this.state.count + 1,
+              person
+            })
+          }}>
+          Add
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
